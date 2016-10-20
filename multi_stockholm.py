0d320e0873addc9e87819fa31e554d8b07daf947
jba462 <jba462@yahoo.com>
2016-10-20 18:47:49 -0400

    added helpful comments

--- multi_stockholm.py
+++ multi_stockholm.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 
+#This script takes many fasta alignment files (MSA's) and writes them
+#all to a single stockholm file suitable for analysis in X-rate.  
 
+#Usage similar to 'cp' command:
+#
+#~$multi_stockholm.py MSA1 MSA2 MSA3... outfile.stock
+#
+#OR
+#
+#~$multi_stockholm.py *.fas outfile.stock (all files ending in .fas)
 
 import sys
 import os
@@ -8,11 +17,19 @@ import os
 from Bio.Alphabet import generic_protein
 from Bio import AlignIO
 
+#THIS CODE ACTUALLY DOES THE WORK
+########################################################################
 msa_files = []
 
 for msa_file in sys.argv[1:-1]:
 	msa_files.append(AlignIO.read(msa_file, "fasta"))
+#AlignIO.write(msa_files, sys.argv[-1], "stockholm") #user-proofed below
+########################################################################
 
+
+
+#ALL OF THIS OTHER CODE JUST KEEPS YOU FROM RUINING YOUR LIFE
+########################################################################
 if os.path.isfile(sys.argv[-1]):
 	user_response = raw_input("Warning! destination file %s already exists. Overwrite (Y/n)?" % sys.argv[-1])
 	
@@ -29,3 +46,4 @@ if os.path.isfile(sys.argv[-1]):
 		
 else:
 	AlignIO.write(msa_files, sys.argv[-1], "stockholm")
+########################################################################
